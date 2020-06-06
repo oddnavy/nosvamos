c88cc5e24425000ecd510a0cbcfe634a1bd7663d
David Pike <david.pike@inlight.com.au>
2020-06-06 23:25:05 +1000

    fix: remove cloudinary context

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
 import '../styles/index.css'
 
-import { CloudinaryContext } from 'cloudinary-react'
-
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <CloudinaryContext
-      cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}
-    >
-      <Component {...pageProps} />
-    </CloudinaryContext>
-  )
+  return <Component {...pageProps} />
 }
